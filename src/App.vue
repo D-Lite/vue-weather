f534dded7202876c0040c7c5b380a9c288b22ac5
Olabemiwo Daniel O <olabemiwodaniel@gmail.com>
2021-01-19 14:52:14 +0100

    Updated, best tried out in mobile view!

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,26 @@
 <template>
-<div id="app">
+<div id="app" :class="typeof weather.main != 'undefined' && weather.main.temp > 16 ? 'warm' : ''">
   <main>
     <div class="search-box">
       <input 
         type="text" 
         class="search-bar"
-        placeholder="Search......."
-        v-model=""
+        placeholder="Search....."
+        v-model="query"
+        @keypress="fetchWeather"
         />
+        
     </div>
 
-    <div class="weather-wrap">
+    <div class="weather-wrap" v-if="typeof weather.main != 'undefined' ">
       <div class="location-box">
-        <div class="location">Lagos ,NG</div>
-        <div class="date">Tuesday, 19 January 2020</div>
+        <div class="location">{{ weather.name }}, {{ weather.sys.country }}</div>
+        <div class="date"> {{ dateBuilder() }} </div>
       </div>
 
       <div class="weather-box">
-        <div class="temp">29°</div>
-        <div class="weather">Hot</div>
+        <div class="temp">{{ Math.round(weather.main.temp) }}°C</div>
+        <div class="weather"> {{ weather.weather[0].main }} </div>  
       </div>
     </div>
   </main>
@@ -39,7 +41,30 @@ export default {
   },
 
   methods: {
+    fetchWeather (e) {
+      if(e.key == "Enter"){
+        fetch(`${this.url_base}weather?q=${this.query}&units=metric&APPID=${this.api_key}`)
+        .then(res => {
+          return res.json();
+        }).then(this.setResults);
+      }
+    },
+    setResults(results){
+      this.weather = results;
+    },
+    dateBuilder(){
+      let d = new Date();
+      let months = ["January", "February", "March", "April",
+       "May", "June","July","August","September", "October", "November", "December"];
+      let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+      let day = days[d.getDay()];
+      let date = d.getDate();
+      let month = months[d.getMonth()];
+      let year = d.getFullYear();
     
+      return `${day} ${date} ${month} ${year}`;
+    }   
   }
 
 }
@@ -64,7 +89,6 @@ body{
 main{
   min-height: 100vh;
   padding: 35px;
-
   background-image: linear-gradient(to bottom, rgba(0,0,0,0.25), rgba(0,0,0,0.75)) ;
 }
 
@@ -72,6 +96,7 @@ main{
   width: 100%;
   margin-bottom: 20px;
   margin-left: 2%;
+  margin-right: 5%;
 }
 
 .search-box .search-bar {
@@ -106,6 +131,10 @@ main{
   font-weight: 500;
   text-align: center;
   text-shadow: rgba(0, 0, 0, 0.25);
+}
+
+#app.warm{
+  background-image: url('./assets/warm-bg.jpg');
 }
 
 .location-box .date{
