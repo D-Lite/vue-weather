b8ec1814eb14ebdbc880e4c63a9816f4bde12b72
Daniel Olabemiwo <44473671+D-Lite@users.noreply.github.com>
2021-01-20 11:40:01 +0100

    Google Analytics

--- src/main.js
+++ src/main.js
@@ -1,4 +1,13 @@
 import { createApp } from 'vue'
+import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+import VueAnalytics from 'vue-analytics';
+
 
 createApp(App).mount('#app')
+
+Vue.use(VueAnalytics, {
+    id: 'UA-259472717',
+    router
+}); 
